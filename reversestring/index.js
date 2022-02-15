c7f693db062265ec7ef82757d1b7f8142dfd7e61
Ojasvi Soni <ojasvisoni20@gmail.com>
2022-02-14 22:52:32 -0500

    first commit

---
+++ reversestring/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    // return str.split('').reverse().join('');
+
+    /* const arr = str.split('');
+       arr.reverse();
+       arr.join(''); 
+    */
+
+    let reversed = '';
+
+    for(let character of str){
+        reversed = character + reversed;
+    }
+
+    return reversed;
+
+    }
+
+module.exports = reverse;
